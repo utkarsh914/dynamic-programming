d5733f97547c796ad6ff92391d46740d6b32afce
Utkarsh Tiwari <utiwari914@gmail.com>
2020-09-10 09:56:39 +0530

    add more problems

--- coin-change.cpp
+++ coin-change.cpp
@@ -36,22 +36,62 @@ Testcase 1: The possiblities are as such: {1, 1, 1, 1}, {1, 1, 2}, {1, 3}, {2, 2
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+/*
+	every time we have a choice of using the current coin
+	and not using it.
+	if using it, then subtract the val of coin from total amount,
+	and ans is the ans for total-coin val
+	when not using it, ans is for previously used coins
+	add both answers to get ans with current coin
+*/
 
 int change(int coins[], int m, int n) {
+
 	int dp[m+1][n+1];
+	// 1 way for making change for 0 amount
 	for (int i=0; i<=m; i++) dp[i][0]=1;
+	// no ways if no coins are given
 	for (int i=1; i<=n; i++) dp[0][i]=0;
 
 	for (int i=1; i<=m; i++) {
 		for (int j=1; j<=n; j++) {
+			// use the curr coin -> dp[i][j-coins[i-1]]
 			int temp = j-coins[i-1];
 			if (temp>=0) temp = dp[i][temp];
 			else temp=0;
+			// don't use curr coin -> dp[i-1][j]
+			// total is addition of both
 			dp[i][j] = dp[i-1][j] + temp;
 		}
 	}
 	return dp[m][n];
 }
+
+// ============ ALTERNATIVE EFFICIENT SOLUTION ==========
+
+int count( int S[], int m, int n ) 
+{ 
+	// table[i] will be storing the number of solutions for 
+	// value i. We need n+1 rows as the table is constructed 
+	// in bottom up manner using the base case (n = 0) 
+	int table[n+1]; 
+
+	// Initialize all table values as 0 
+	memset(table, 0, sizeof(table)); 
+
+	// Base case (If given value is 0) 
+	table[0] = 1; 
+
+	// Pick all coins one by one and update the table[] values 
+	// after the index greater than or equal to the value of the 
+	// picked coin 
+	for(int i=0; i<m; i++) 
+		for(int j=S[i]; j<=n; j++) 
+			table[j] += table[j-S[i]];
+
+	return table[n]; 
+} 
+
 
 int main() {
 	int t; cin>>t;
--- longest-common-substring.cpp
+++ longest-common-substring.cpp
@@ -39,13 +39,13 @@ using namespace std;
 int lcs(string s1, string s2, int m, int n) {
 	int dp[m+1][n+1];
 	int result=0;
-	memset(dp, -1, sizeof(dp));
+	
 	for (int i=0; i<=m; i++) {
 		for (int j=0; j<=n; j++) {
 			if (i==0 || j==0) dp[i][j] = 0;
 			else if (s1[i-1]==s2[j-1]) {
 				dp[i][j] = 1 + dp[i-1][j-1];
-				if (dp[i][j] > result) result = dp[i][j];
+				result = max(result, dp[i][j]);
 			}
 			else dp[i][j] = 0;
 		}
